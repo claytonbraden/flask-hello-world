fd8b9584ebbd266317bcc9b8de5531dce3df65f9
Clayton Braden <claytonbraden1@gmail.com>
2025-03-24 00:29:41 +0000

    Added basketball table creation to app.py

--- app.py
+++ app.py
@@ -11,4 +11,22 @@ def hello_world():
 def db_test():
     conn = psycopg2.connect("postgresql://lab_10_claytonbraden_db_user:oCclnkfeDRkdEGjWij43wCIVuQ5g0GvD@dpg-cvg95flrie7s73bnf35g-a.oregon-postgres.render.com/lab_10_claytonbraden_db")
     conn.close()
-    return 'Database Connection sucessful;'
+    return 'Database Connection sucessful'
+
+@app.route('/db_create')
+def db_create():
+    cur = conn.cursor()
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS Basketball(
+        First varchar(255),
+        Last varchar(255),
+        City varchar(255),
+        Name varchar(255),
+        Number int
+        );
+''')
+    conn.commit()
+    conn.close()
+    return 'Basketball Table Succesfully Created'
+
+    
