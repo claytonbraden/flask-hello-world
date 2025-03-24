2b9e0a576195d5f85aa1ae0b8dacd07a6a9844b0
Clayton Braden <claytonbraden1@gmail.com>
2025-03-24 00:03:18 +0000

    Added database conenction

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask
+import psycopg2
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World from claytonbraden in 3308'
+
+@app.route('/db_test')
+def db_test():
+    conn = psycopg2.connect("postgresql://lab_10_claytonbraden_db_user:oCclnkfeDRkdEGjWij43wCIVuQ5g0GvD@dpg-cvg95flrie7s73bnf35g-a/lab_10_claytonbraden_db")
+    conn.close()
+    return 'Database Connection sucessful;'
